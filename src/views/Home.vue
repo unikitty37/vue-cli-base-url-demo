8dc3b09ca07623bf41f03aa5aabd710bc6a55cfb
John Yeates <unikitty37@users.noreply.github.com>
2020-08-26 09:53:07 +0100

    Initial

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    Base URL is <strong>{{ baseUrl }}</strong>.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+
+  computed: {
+    baseUrl () {
+      return process.env.BASE_URL
+    }
+  }
+}
+</script>
